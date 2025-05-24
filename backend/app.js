b3f8048f25f1eb31f43cad01e09c0f768ac31ceb
Rishav Chapagain <gainrishavchap@gmail.com>
2025-05-24 21:37:15 +0545

    Minor Fixes

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ import { logger } from './middlewares/middlewares.js';
 import authRouter from './routes/authrouter.js';
 const app = express()
 app.use(logger)
+app.use(express.json())
 app.use('/api/v1/auth',authRouter)
 
 export default app
