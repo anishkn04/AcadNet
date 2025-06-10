b6f375d628fe1a1f796baf4188aee1de2629d1e3
Rishav Chapagain <gainrishavchap@gmail.com>
2025-06-10 19:27:16 +0545

    Feat: GitHub Oauth Added

--- backend/middlewares/csrf.js
+++ backend/middlewares/csrf.js
@@ -2,11 +2,11 @@ import jsonRes from "../utils/response.js";
 
 const csrfMiddleware = (req,res,next)=>{
   const csrfCookie = req.cookies.csrfToken;
-  console.log("Reached Here")
+
   const csrfHeader = req.headers['x-csrf-token']
-  console.log("Reached Here1")
+
   console.log(csrfCookie)
-  console.log("Reached Here2")
+
      if (!csrfCookie || !csrfHeader || csrfCookie !== csrfHeader) {
     return jsonRes(res, 403, false, "Invalid Token");
   }
