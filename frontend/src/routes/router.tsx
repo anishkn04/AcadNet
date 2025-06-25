0183244c27cdca269c06b04566f5c804c3438875
Nishan2004paudel <neezpau04@gmail.com>
2025-06-25 14:12:46 +0545

    path updated and protected of user profile

--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -38,7 +38,11 @@ export const router = createBrowserRouter([
   },
   {
     path: "/user",
-    element: <UserLayout />,
+    element: (
+      <ProtectedRoutes>
+        <UserLayout />
+      </ProtectedRoutes>
+    ),
     children: [{ index: true, element: <UserProfile /> }],
   },
 
