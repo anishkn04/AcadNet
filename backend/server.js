22b5273336682003e8f2f0be272b5432c45e2007
Rishav Chapagain <gainrishavchap@gmail.com>
2025-07-09 21:08:54 +0545

    fix: minor bugs

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const startServer = async () => {
     await sequelize.authenticate();
     console.log('✅ Database connected successfully');
 
-    await sequelize.sync({force: true})
+    await sequelize.sync({alter: true})
 
     console.log('✅ Models synced successfully');
 
